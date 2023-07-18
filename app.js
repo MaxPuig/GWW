f431163fe1fe6e552ee134d4a4dad05a984f7b22
MaxPuig <45133421+MaxPuig@users.noreply.github.com>
2023-07-18 07:54:30 +0200

    Add: download p2shdata file

--- app.js
+++ app.js
@@ -223,7 +223,7 @@ function wssUTXO(address) {
   });
 }
 
-function wssTXID(txid) {
+function wssTXID(txid, parsed = true) {
   return new Promise((resolve, reject) => {
     socket.onerror = function (error) {
       reject(error);
@@ -232,7 +232,7 @@ function wssTXID(txid) {
       JSON.stringify({
         id: 0,
         method: "blockchain.transaction.get",
-        params: [txid, true],
+        params: [txid, parsed],
       })
     );
     socket.onmessage = function (event) {
@@ -335,9 +335,117 @@ function generateAddress(password) {
   });
 }
 
+/* P2SHDATA */
+
+async function getP2SHDATA(txid) {
+  if (socket?.readyState !== 1) {
+    document.getElementById("downloadp2shdata").innerHTML = "Not connected to Electrum server";
+    return;
+  }
+  document.getElementById("downloadp2shdata").innerHTML = "Loading...";
+  const response = await wssTXID(txid, false);
+  if (response.error?.message) {
+    document.getElementById("downloadp2shdata").innerHTML = JSON.stringify(response.error.message);
+  } else {
+    let rawTx = response.result;
+    let tx = garlicore.Transaction(rawTx).toObject();
+    let op_return = tx.outputs.filter((vout) => { return vout.script.startsWith('6a') })[0] || { script: '' };
+    if (hexToAscii(op_return.script.slice(30, 50)).replace(/\x00/g, '') != '/p2shdata') {
+      document.getElementById("downloadp2shdata").innerHTML = "Not a P2SHDATA transactio";
+      return;
+    }
+    let title = tx.outputs.filter((vout) => { return vout.satoshis == 0 })[0].script;
+    let data_array = tx.inputs.map((vin) => { return vin.script });
+    let data = '';
+    for (let chunk of data_array) {
+      data += cutScript(cutScript(chunk));
+    }
+    let decodedTitle = decodeTitle(title);
+    const filename = `${decodedTitle.filename}.${decodedTitle.filetype}`;
+    document.getElementById("downloadp2shdata").innerHTML = JSON.stringify(decodedTitle, null, 2);
+    downloadBufferAsFile(Buffer.from(data, "hex"), filename)
+  }
+}
+
+function decodeTitle(vout_string) {
+  let hex = vout_string.slice(6); // remove the first 3 bytes (OP_CODES)
+  let site = hexToAscii(hex.slice(0, 24)).replace(/\x00/g, '');
+  let protocol = hexToAscii(hex.slice(24, 44)).replace(/\x00/g, '');
+  let version = hexToDecimal(hex.slice(44, 48));
+  let filename = hexToAscii(hex.slice(48, 80)).replace(/\x00/g, '');
+  let filetype = hexToAscii(hex.slice(80, 88)).replace(/\x00/g, '');
+  let filesize = hexToDecimal(hex.slice(88, 96));
+  let assembly_script = hex.slice(96, 120);
+  let datahash160 = hex.slice(120, 160);
+  let info = { site, protocol, version, filename, filetype, filesize, assembly_script, datahash160 };
+  info.assembly_script = decodeAssemblyScript(assembly_script);
+  return info;
+}
+
+function decodeAssemblyScript(entire_assembly_script) {
+  let assembly_script_length = hexToDecimal(entire_assembly_script.slice(0, 2));
+  let script = entire_assembly_script.slice(2, assembly_script_length * 2 + 2);
+  let data_location = script.slice(0, 6);
+  let first_vin = hexToDecimal(script.slice(2, 4));
+  let last_vin = hexToDecimal(script.slice(4, 6));
+  let encoding_type = 'ASCII';
+  let encoding;
+  if (script.includes('ec')) {
+    encoding = script.slice(6, 10);
+    encoding_type = encoding.slice(2, 4);
+    if (encoding_type == '64') {
+      encoding_type = 'base64';
+    } else if (encoding_type == '16') {
+      encoding_type = 'hex';
+    } else if (encoding_type == '10') {
+      encoding_type = 'base10';
+    } else if (encoding_type == 'f8') {
+      encoding_type = 'UTF-8';
+    } else {
+      encoding_type = 'ASCII';
+    }
+  }
+  let info = { entire_assembly_script, assembly_script_length, script, data_location, first_vin, last_vin, encoding_type };
+  if (encoding) info.encoding = encoding;
+  return info;
+}
+
+function hexToAscii(hex) { return Buffer.from(hex, 'hex').toString(); }
+
+function hexToDecimal(hex) { return parseInt(hex, 16); }
+
+function littleEndianToDecimal(hex) { return parseInt(hex.match(/.{2}/g).reverse().join(''), 16); }
+
+function cutScript(chunk) {
+  let data = '';
+  if (chunk.startsWith('4d')) { // OP_PUSHDATA2 + 2 bytes little endian length
+    let length = littleEndianToDecimal(chunk.slice(2, 6)) * 2;
+    data += chunk.slice(6, length + 6);
+  } else if (chunk.startsWith('4c')) { // OP_PUSHDATA1 + 1 byte length
+    let length = hexToDecimal(chunk.slice(2, 4)) * 2;
+    data += chunk.slice(4, length + 4);
+  } else { // Pushdata Bytelengh 1-75
+    let length = hexToDecimal(chunk.slice(0, 2)) * 2;
+    data += chunk.slice(2, length + 2);
+  }
+  return data;
+}
+
+function downloadBufferAsFile(buffer, filename) {
+  const blob = new Blob([buffer]);
+  const anchorElement = document.createElement('a');
+  anchorElement.href = window.URL.createObjectURL(blob);
+  anchorElement.download = filename;
+  anchorElement.click();
+  window.URL.revokeObjectURL(anchorElement.href);
+}
+
+/* END P2SHDATA */
+
 module.exports = {
   connectToElectrum,
   createTransaction,
+  getP2SHDATA,
   getBalance,
   getUTXO,
   getTXID,
